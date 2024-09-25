ec2f00981a6e1fe8abcbb3865ea8e38109330849
Raymond Dave Guibone <mymails.dev@gmail.com>
2024-09-25 21:17:29 +0800

    My Personal Portfolio

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// Typing animation
+var typed = new Typed('.typing', {
+  strings: [
+    '',
+    'Front-end Developer',
+    'Quality Assurance Tester',
+    'Web Developer',
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
